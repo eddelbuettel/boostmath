92bf7e2facc2a627f1b8a44bd2053937bafafe58
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-07-01 19:48:27 +0800

    Split hankel for gcc

---
+++ src/sph_hankel_2.cpp
@@ -0,0 +1,7 @@
+#include <cpp11.hpp>
+#include <cpp11/declarations.hpp>
+#include <boost/math/special_functions/hankel.hpp>
+#include "boostmath/complex_interop.hpp"
+#include "boostmath/macros.hpp"
+
+BINARY_BOOST_FUNCTION(sph_hankel_2, double, double)
