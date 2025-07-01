67b2bac304742f79dc5506f8ddfa14482728aa9e
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-07-01 14:14:43 +0800

    consts

--- src/boostmath/macros.hpp
+++ src/boostmath/macros.hpp
@@ -6,7 +6,7 @@
 #define NOARG_BOOST_FUNCTION(name) \
   extern "C" SEXP name##_() { \
     BEGIN_CPP11 \
-    auto result = boost::math::name(); \
+    const auto result = boost::math::name(); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -14,8 +14,8 @@
 #define UNARY_BOOST_FUNCTION(name, arg_type) \
   extern "C" SEXP name##_(SEXP x_) { \
     BEGIN_CPP11 \
-    arg_type x = boostmath::as_cpp<arg_type>(x_); \
-    auto result = boost::math::name<double>(x); \
+    const arg_type x = boostmath::as_cpp<arg_type>(x_); \
+    const auto result = boost::math::name<double>(x); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -23,8 +23,8 @@
 #define UNARY_BOOST_FUNCTION_SUFFIX(name, suffix, arg_type) \
   extern "C" SEXP name##_##suffix(SEXP x_) { \
     BEGIN_CPP11 \
-    arg_type x = boostmath::as_cpp<arg_type>(x_); \
-    auto result = boost::math::name(x); \
+    const arg_type x = boostmath::as_cpp<arg_type>(x_); \
+    const auto result = boost::math::name(x); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -32,9 +32,9 @@
 #define BINARY_BOOST_FUNCTION(name, arg1_type, arg2_type) \
   extern "C" SEXP name##_(SEXP x_, SEXP y_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    auto result = boost::math::name(x, y); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const auto result = boost::math::name(x, y); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -42,9 +42,9 @@
 #define BINARY_BOOST_FUNCTION_SUFFIX(name, suffix, arg1_type, arg2_type) \
   extern "C" SEXP name##_##suffix(SEXP x_, SEXP y_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    auto result = boost::math::name(x, y); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const auto result = boost::math::name(x, y); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -52,10 +52,10 @@
 #define TERNARY_BOOST_FUNCTION(name, arg1_type, arg2_type, arg3_type) \
   extern "C" SEXP name##_(SEXP x_, SEXP y_, SEXP z_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
-    auto result = boost::math::name(x, y, z); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
+    const auto result = boost::math::name(x, y, z); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -63,10 +63,10 @@
 #define TERNARY_BOOST_FUNCTION_SUFFIX(name, suffix, arg1_type, arg2_type, arg3_type) \
   extern "C" SEXP name##_##suffix(SEXP x_, SEXP y_, SEXP z_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
-    auto result = boost::math::name(x, y, z); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
+    const auto result = boost::math::name(x, y, z); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -74,11 +74,11 @@
 #define QUARTERNARY_BOOST_FUNCTION(name, arg1_type, arg2_type, arg3_type, arg4_type) \
   extern "C" SEXP name##_(SEXP x_, SEXP y_, SEXP z_, SEXP w_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
-    arg4_type w = boostmath::as_cpp<arg4_type>(w_); \
-    auto result = boost::math::name(x, y, z, w); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
+    const arg4_type w = boostmath::as_cpp<arg4_type>(w_); \
+    const auto result = boost::math::name(x, y, z, w); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -86,9 +86,9 @@
 #define BINARY_DISTRIBUTION_BOOST_IMPL(func, dist, arg1_type, arg2_type) \
   extern "C" SEXP dist##_##func##_(SEXP x_, SEXP y_, SEXP z_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    auto result = boost::math::func(boost::math::dist##_distribution<>(y), x); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const auto result = boost::math::func(boost::math::dist##_distribution<>(y), x); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -103,10 +103,10 @@
 #define TERNARY_DISTRIBUTION_BOOST_IMPL(func, dist, arg1_type, arg2_type, arg3_type) \
   extern "C" SEXP dist##_##func##_(SEXP x_, SEXP y_, SEXP z_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
-    auto result = boost::math::func(boost::math::dist##_distribution<>(y, z), x); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
+    const auto result = boost::math::func(boost::math::dist##_distribution<>(y, z), x); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
@@ -121,11 +121,11 @@
 #define QUARTERNARY_DISTRIBUTION_BOOST_IMPL(func, dist, arg1_type, arg2_type, arg3_type, arg4_type) \
   extern "C" SEXP dist##_##func##_(SEXP x_, SEXP y_, SEXP z_, SEXP w_) { \
     BEGIN_CPP11 \
-    arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
-    arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
-    arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
-    arg4_type w = boostmath::as_cpp<arg4_type>(w_); \
-    auto result = boost::math::func(boost::math::dist##_distribution<>(y, z, w), x); \
+    const arg1_type x = boostmath::as_cpp<arg1_type>(x_); \
+    const arg2_type y = boostmath::as_cpp<arg2_type>(y_); \
+    const arg3_type z = boostmath::as_cpp<arg3_type>(z_); \
+    const arg4_type w = boostmath::as_cpp<arg4_type>(w_); \
+    const auto result = boost::math::func(boost::math::dist##_distribution<>(y, z, w), x); \
     return boostmath::as_sexp(result); \
     END_CPP11 \
   }
