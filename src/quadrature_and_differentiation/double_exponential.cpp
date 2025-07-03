d029fd4e1ab4a8a6b0658cbbc178c5dfaa7d7cc0
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-07-03 14:57:01 +0800

    Returns outside blocks

--- src/quadrature_and_differentiation/double_exponential.cpp
+++ src/quadrature_and_differentiation/double_exponential.cpp
@@ -11,52 +11,92 @@
 extern "C" {
   SEXP tanh_sinh_(SEXP f_, SEXP a_, SEXP b_, SEXP tol_, SEXP max_refinements_) {
     double result = std::numeric_limits<double>::quiet_NaN();
-    BEGIN_CPP11
-    cpp11::function f(f_);
-    double a = cpp11::as_cpp<double>(a_);
-    double b = cpp11::as_cpp<double>(b_);
-    double tol = cpp11::as_cpp<double>(tol_);
-    size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
+    SEXP err = R_NilValue;
+    char buf[8192] = "";
+    try {
+      cpp11::function f(f_);
+      double a = cpp11::as_cpp<double>(a_);
+      double b = cpp11::as_cpp<double>(b_);
+      double tol = cpp11::as_cpp<double>(tol_);
+      size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
 
-    auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
+      auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
 
-    boost::math::quadrature::tanh_sinh<double> integrator(max_refinements);
-    result = integrator.integrate(func, a, b, tol);
-    END_CPP11
+      boost::math::quadrature::tanh_sinh<double> integrator(max_refinements);
+      result = integrator.integrate(func, a, b, tol);
+    } catch (cpp11::unwind_exception & e) {
+      err = e.token;
+    } catch (std::exception & e) {
+      strncpy(buf, e.what(), sizeof(buf) - 1);
+    } catch (...) {
+      strncpy(buf, "C++ error (unknown cause)", sizeof(buf) - 1);
+    }
+
+    if (buf[0] != '\0') {
+      Rf_errorcall(R_NilValue, "%s", buf);
+    } else if (err != R_NilValue) {
+      R_ContinueUnwind(err);
+    }
     return cpp11::as_sexp(result);
   }
 
   SEXP sinh_sinh_(SEXP f_, SEXP tol_, SEXP max_refinements_) {
     double result = std::numeric_limits<double>::quiet_NaN();
-    BEGIN_CPP11
-    cpp11::function f(f_);
-    double tol = cpp11::as_cpp<double>(tol_);
-    size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
+    SEXP err = R_NilValue;
+    char buf[8192] = "";
+    try {
+      cpp11::function f(f_);
+      double tol = cpp11::as_cpp<double>(tol_);
+      size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
 
-    auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
+      auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
 
-    boost::math::quadrature::sinh_sinh<double> integrator(max_refinements);
-    result = integrator.integrate(func, tol);
+      boost::math::quadrature::sinh_sinh<double> integrator(max_refinements);
+      result = integrator.integrate(func, tol);
+    } catch (cpp11::unwind_exception & e) {
+      err = e.token;
+    } catch (std::exception & e) {
+      strncpy(buf, e.what(), sizeof(buf) - 1);
+    } catch (...) {
+      strncpy(buf, "C++ error (unknown cause)", sizeof(buf) - 1);
+    }
 
-    END_CPP11
+    if (buf[0] != '\0') {
+      Rf_errorcall(R_NilValue, "%s", buf);
+    } else if (err != R_NilValue) {
+      R_ContinueUnwind(err);
+    }
     return cpp11::as_sexp(result);
   }
 
   SEXP exp_sinh_(SEXP f_, SEXP a_, SEXP b_, SEXP tol_, SEXP max_refinements_) {
     double result = std::numeric_limits<double>::quiet_NaN();
-    BEGIN_CPP11
-    cpp11::function f(f_);
-    double a = cpp11::as_cpp<double>(a_);
-    double b = cpp11::as_cpp<double>(b_);
-    double tol = cpp11::as_cpp<double>(tol_);
-    size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
+    SEXP err = R_NilValue;
+    char buf[8192] = "";
+    try {
+      cpp11::function f(f_);
+      double a = cpp11::as_cpp<double>(a_);
+      double b = cpp11::as_cpp<double>(b_);
+      double tol = cpp11::as_cpp<double>(tol_);
+      size_t max_refinements = cpp11::as_cpp<size_t>(max_refinements_);
 
-    auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
+      auto func = [&f](double x) { return cpp11::as_cpp<double>(f(x)); };
 
-    boost::math::quadrature::exp_sinh<double> integrator(max_refinements);
-    result = integrator.integrate(func, a, b, tol);
+      boost::math::quadrature::exp_sinh<double> integrator(max_refinements);
+      result = integrator.integrate(func, a, b, tol);
+    } catch (cpp11::unwind_exception & e) {
+      err = e.token;
+    } catch (std::exception & e) {
+      strncpy(buf, e.what(), sizeof(buf) - 1);
+    } catch (...) {
+      strncpy(buf, "C++ error (unknown cause)", sizeof(buf) - 1);
+    }
 
-    END_CPP11
+    if (buf[0] != '\0') {
+      Rf_errorcall(R_NilValue, "%s", buf);
+    } else if (err != R_NilValue) {
+      R_ContinueUnwind(err);
+    }
     return cpp11::as_sexp(result);
   }
 }
